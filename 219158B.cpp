f040bef75c74aa0b74bd9440dbfd4fabf68d3230
Mahmoud Abo-Zied <mahmoudkailad4444@gmail.com>
2025-10-31 23:04:47 +0200

    Add 31 October'25 Sols

---
+++ 219158B.cpp
@@ -0,0 +1,139 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define ll long long
+#define int long long
+typedef unsigned long long ull;
+#define endl '\n'
+typedef pair<int, int> pii;
+#define elif else if
+#define endd(s) return void(cout << s << "\n")
+#define Ceil(n, m) (((n) / (m)) + ((n) % (m) ? 1 : 0))
+#define X first
+#define Y second
+#define fixed(n) fixed << setprecision(n)
+#define sz(s) int32_t(s.size())
+#define dbg(x) cout << #x << ": " << (x) << "\n";
+#define getline(s) getline(cin >> ws, s)
+#define Time cerr << "Time Taken: " << (float)clock() / CLOCKS_PER_SEC << " Secs" << "\n";
+#define all(vec) vec.begin(), vec.end()
+#define rall(v) v.rbegin(),v.rend()
+#define mul_mod(a, b) (((a % M) * (b % M)) % M)
+#define add_mod(a, b) (((a % M) + (b % M)) % M)
+#define sub_mod(a, b) (((a % M) - (b % M) + M) % M)
+const int N = 2e5 + 10, M = 1'000'000'007, OO = 0X3F3F3F3F3F3F3F3F;
+const double EPS = 1e-9, pi = 3.141592653589793;
+#define kill return 0
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+#define pb push_back
+vector<string> RET = {"NO", "YES"};
+#define IO(NAME) \
+cin.tie(0)->sync_with_stdio(0); \
+if(fopen(NAME ".in","r")) freopen(NAME ".in","r",stdin), \
+freopen(NAME ".out","w",stdout);
+template<class T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+void Zied() {
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout);
+#endif
+}
+
+template<typename T = int>
+istream &operator >>(istream &in, vector<T> &v) {
+    for (auto &x: v) in >> x;
+    return in;
+}
+
+template<typename T = int>
+ostream &operator <<(ostream &out, const vector<T> &v) {
+    for (const T &x: v) out << x << ' ';
+    return out;
+}
+
+template<typename A, typename B>
+istream &operator>>(istream &fin, pair<A, B> &p) {
+    fin >> p.X >> p.Y;
+    return fin;
+}
+
+template<typename A, typename B>
+std::ostream &operator<<(std::ostream &fout, const std::pair<A, B> &p) {
+    fout << p.first << " " << p.second;
+    return fout;
+}
+
+enum dir { d, r, u, l, ul, dr, ur, dl };
+
+int dx[8] = {1, 0, -1, 0, -1, 1, -1, 1};
+int dy[8] = {0, 1, 0, -1, -1, 1, 1, -1};
+
+
+void preprocessing() {
+}
+
+
+auto Solve(const int &n) -> void {
+    int a;
+    long long b;
+    char c;
+    float d;
+    double e;
+
+    cin >> a >> b  >>  c >> d >> e;
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << d << endl;
+    cout << e << endl;
+
+}
+
+bool solve_test(const int test_number) {
+    int n = 1;
+    // string n;
+    // if (!(cin >> n))
+    //     return false;
+    Solve(n);
+    // auto ans = Solve(n);
+    // cout << ans << endl;
+    return true;
+}
+
+void Stress() {
+    // for (int n = 2; n <= 1; ++n)
+    //     cerr << n << ' ' << Solve(n) << '\n';
+}
+
+int32_t main() {
+    Zied();
+    Stress();
+    // freopen("document.in", "r", stdin);
+    // freopen("document.out", "w", stdout);
+    preprocessing();
+    int test_cases = 1;
+    cin >> test_cases;
+    for (int tc = 1; tc <= test_cases; tc++) {
+        // cout << "Case " << tc << ": " << endl;
+        cout << "Case #" << tc << ": ";
+        if (!solve_test(tc))break;
+        // cout << endl;
+    }
+    kill;
+    //    Time
+}
+
+/*
+  _____       _ _                      _______ 
+ |  ___|     (_) |                    |___  (_)        | |
+ | |__ _ __  _| |_ ___  _ __ ___   ___   / / _  ___  __| |
+ |  __| '_ \| | __/ _ \| '_ ` _ \ / _ \ / / | |/ _ \/ _` |
+ | |__| |_) | | || (_) | | | | | |  __/  /__| |  __/ (_| |
+ \____/ .__/|_|\__\___/|_| |_| |_|\___ \_____/_|\___|\__,_|
+      | |                                                  
+      |_|                                                  
+*/
---
+++ Counting_Rooms.cpp
@@ -0,0 +1,159 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define ll long long
+#define int long long
+typedef unsigned long long ull;
+#define endl '\n'
+typedef pair<int, int> pii;
+#define elif else if
+#define endd(s) return void(cout << s << "\n")
+#define Ceil(n, m) (((n) / (m)) + ((n) % (m) ? 1 : 0))
+#define X first
+#define Y second
+#define fixed(n) fixed << setprecision(n)
+#define sz(s) int32_t(s.size())
+#define dbg(x) cout << #x << ": " << (x) << "\n";
+#define getline(s) getline(cin >> ws, s)
+#define Time cerr << "Time Taken: " << (float)clock() / CLOCKS_PER_SEC << " Secs" << "\n";
+#define all(vec) vec.begin(), vec.end()
+#define rall(v) v.rbegin(),v.rend()
+#define mul_mod(a, b) (((a % M) * (b % M)) % M)
+#define add_mod(a, b) (((a % M) + (b % M)) % M)
+#define sub_mod(a, b) (((a % M) - (b % M) + M) % M)
+const int N = 2e5 + 10, M = 1'000'000'007, OO = 0X3F3F3F3F3F3F3F3F;
+const double EPS = 1e-9, pi = 3.141592653589793;
+#define kill return 0
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+#define pb push_back
+vector<string> RET = {"NO", "YES"};
+#define IO(NAME) \
+cin.tie(0)->sync_with_stdio(0); \
+if(fopen(NAME ".in","r")) freopen(NAME ".in","r",stdin), \
+freopen(NAME ".out","w",stdout);
+template<class T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+void Zied() {
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout);
+#endif
+}
+
+template<typename T = int>
+istream &operator >>(istream &in, vector<T> &v) {
+    for (auto &x: v) in >> x;
+    return in;
+}
+
+template<typename T = int>
+ostream &operator <<(ostream &out, const vector<T> &v) {
+    for (const T &x: v) out << x << ' ';
+    return out;
+}
+
+template<typename A, typename B>
+istream &operator>>(istream &fin, pair<A, B> &p) {
+    fin >> p.X >> p.Y;
+    return fin;
+}
+
+template<typename A, typename B>
+std::ostream &operator<<(std::ostream &fout, const std::pair<A, B> &p) {
+    fout << p.first << " " << p.second;
+    return fout;
+}
+
+enum dir { d, r, u, l, ul, dr, ur, dl };
+
+int dx[8] = {1, 0, -1, 0, -1, 1, -1, 1};
+int dy[8] = {0, 1, 0, -1, -1, 1, 1, -1};
+
+
+void preprocessing() {
+}
+
+
+auto Solve(const int &n) -> void {
+    int m;
+    cin >> m;
+    vector<vector<char> > grid(n, vector<char>(m));
+     for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> grid[i][j];
+        }
+    }
+
+    function<bool(int, int)> valid = [&](int u, int v) {
+        return u >= 0 and u < sz(grid) and v >= 0 and v < sz(grid[0]) and grid[u][v] != '#';
+    };
+
+    function<void(int, int)> dfs = [&](int u, int v) {
+        grid[u][v] = '#';
+        for (int k = 0; k < 4; k++) {
+            int nu = u + dx[k], nv = v + dy[k];
+            if (valid(nu, nv)) {
+                dfs(nu, nv);
+            }
+        }
+    };
+
+    int ans = 0 ;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            if (grid[i][j] == '.') {
+                ans ++;
+                dfs(i, j);
+            }
+        }
+    }
+    cout << ans << "\n";
+}
+
+bool solve_test(const int test_number) {
+    int n = 1;
+    // string n;
+    if (!(cin >> n))
+        return false;
+    Solve(n);
+    // auto ans = Solve(n);
+    // cout << ans << endl;
+    return true;
+}
+
+void Stress() {
+    // for (int n = 2; n <= 1; ++n)
+    //     cerr << n << ' ' << Solve(n) << '\n';
+}
+
+int32_t main() {
+    Zied();
+    Stress();
+    // freopen("document.in", "r", stdin);
+    // freopen("document.out", "w", stdout);
+    preprocessing();
+    int test_cases = 1;
+    // cin >> test_cases;
+    for (int tc = 1; tc <= test_cases; tc++) {
+        // cout << "Case " << tc << ": " << endl;
+        // cout << "Case #" << tc << ": ";
+        if (!solve_test(tc))break;
+        // cout << endl;
+    }
+    kill;
+    //    Time
+}
+
+/*
+  _____       _ _                      _______
+ |  ___|     (_) |                    |___  (_)        | |
+ | |__ _ __  _| |_ ___  _ __ ___   ___   / / _  ___  __| |
+ |  __| '_ \| | __/ _ \| '_ ` _ \ / _ \ / / | |/ _ \/ _` |
+ | |__| |_) | | || (_) | | | | | |  __/  /__| |  __/ (_| |
+ \____/ .__/|_|\__\___/|_| |_| |_|\___ \_____/_|\___|\__,_|
+      | |
+      |_|
+*/
